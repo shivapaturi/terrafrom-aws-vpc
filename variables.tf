9267d83413e5a8f8a503594f23cb4a50368f24c0
shiva paturi <shivapaturi01@gmail.com>
2025-08-24 21:58:20 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+    type = list(string)
+}
