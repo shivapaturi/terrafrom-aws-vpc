794178f3540adc4e4d90805572055f0e4484fa6c
shiva paturi <shivapaturi01@gmail.com>
2025-08-31 13:54:08 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -43,3 +43,37 @@ variable "database_subnet_tags" {
     type = map(string)
     default = {}
 }
+
+variable "eip_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "is_peering_required" {
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    type = map(string)
+    default = {}
+}
